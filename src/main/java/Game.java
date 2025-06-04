a5c38016038880dce4e9c2b2c1185caa05c93113
magicers <magicers123@gmail.com>
2025-06-04 14:11:27 +0900

    [feature] p49

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,14 @@
 public class Game {
-    public void guess(String guessNumber) {
+
+    public String question;
+
+    public GuessResult guess(String guessNumber) {
         assertIllegalArgument(guessNumber);
+        if(guessNumber.equals(question)){
+            return new GuessResult(true, 3, 0);
+        }
+
+        return null;
     }
 
     private void assertIllegalArgument(String guessNumber) {
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,6 +1,8 @@
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
@@ -19,12 +21,13 @@ class GameTest {
     private void assertIllegalArgument(String guessNumber) {
         try{
             game.guess(guessNumber);
-            fail();
+            org.assertj.core.api.Assertions.fail();
         }
         catch (IllegalArgumentException e){
 
         }
     }
+
 
     @Test
     void throwIllegalArgumentExceptionInvalidInput() {
@@ -35,5 +38,14 @@ class GameTest {
         assertIllegalArgument("121");
     }
 
+    @Test
+    void returnSolveResultIfMatchNumber() {
+        game.question = "123";
+        GuessResult result = game.guess("123");
 
+        assertThat(result).isNotNull();
+        assertThat(result.solved).isEqualTo(true);
+        assertThat(result.strikes).isEqualTo(3);
+        assertThat(result.balls).isEqualTo(0);
+    }
 }
