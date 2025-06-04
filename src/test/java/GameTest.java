8c4377565c030b19010ce899d5a92ccb02a6dc3f
magicers <magicers123@gmail.com>
2025-06-04 13:51:08 +0900

    [feature] p39

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -32,6 +32,7 @@ class GameTest {
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
         assertIllegalArgument("12s");
+        assertIllegalArgument("121");
     }
 
 
